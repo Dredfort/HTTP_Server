cbd25b93c53e83f85ec7fda5f6dfb80c9e0d7ea7
Oleg Osadchij <oleg.osadchij@murka.com>
2018-03-02 16:36:20 +0200

    logging

--- HTTP_Server/server.js
+++ HTTP_Server/server.js
@@ -1,8 +1,16 @@
+// http://localhost:8888/
 
+
+var serverPort = 8888;
 var htttp = require("http");
 
 htttp.createServer(function (reqest, responce) {
+    
+    console.log(reqest);
+
     responce.writeHead(200, { "Content-Type": "text/plain" });
     responce.write("Hi there");
     responce.end();
-}).listen(8888);
+}).listen(serverPort);
+
+console.log("Server started. Port: %f", serverPort);
