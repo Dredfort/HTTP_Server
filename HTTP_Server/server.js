9d8e9c13edd50f8b3919274e27d076176cd0e630
Oleg Osadchij <oleg.osadchij@murka.com>
2018-03-05 15:49:09 +0200

    [#] add comments

--- HTTP_Server/server.js
+++ HTTP_Server/server.js
@@ -4,29 +4,29 @@
 
 var http = require("http");
 var url = require("url");
-
 function start(serverPortroute, route, handle) {
     function onRequest(request, response) {
-      var postData = "";
-      var pathname = url.parse(request.url).pathname;
-      console.log("Request for " + pathname + " received.");
-  
-      request.setEncoding("utf8");  
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-      request.addListener("data", function(postDataChunk) {
-        postData += postDataChunk;
-        console.log("Received POST data chunk '"+
-        postDataChunk + "'.");
-      });
-  
-      request.addListener("end", function() {
-        route(handle, pathname, response, postData);
-      });
-  
+        request.setEncoding("utf8");
+
+        // data lisetener.
+        request.addListener("data", function (postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" +
+                postDataChunk + "'.");
+        });
+
+        // Handler
+        request.addListener("end", function () {
+            route(handle, pathname, response, postData);
+        });
     }
-  
+
     http.createServer(onRequest).listen(serverPortroute);
     console.log("Server has started. Port [%d]", serverPortroute);
-  }
-  
-  exports.start = start;
+}
+
+exports.start = start;
