8b243df2e073669cd2fb0db62c642f1579cc105e
Oleg Osadchij <oleg.osadchij@murka.com>
2018-03-05 10:46:51 +0200

    [+] add routing. request handling

--- HTTP_Server/server.js
+++ HTTP_Server/server.js
@@ -1,15 +1,18 @@
 // http://localhost:8888/
+// http://localhost:8888/start
+// http://localhost:8888/upload
 
 var htttp = require("http");
 var url = require("url");
 
-function start(serverPort) {
+function start(serverPort, route, handle) {
 
     function onRequest (reqest, responce) {
 
         var pathName = url.parse(reqest.url).pathname;
-
         console.log("Request for [%s] received", pathName);
+
+        route(handle, pathName);
 
         responce.writeHead(200, { "Content-Type": "text/plain" });
         responce.write("Hi bro!");
@@ -17,7 +20,6 @@ function start(serverPort) {
     }
 
     htttp.createServer(onRequest).listen(serverPort);
-
     console.log("Server has started. Port: [%f]", serverPort);
 }
 
