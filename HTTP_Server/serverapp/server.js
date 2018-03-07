274591e6c1b321a4668445a409ef79ce99bbda1a
Oleg Osadchij <oleg.osadchij@murka.com>
2018-03-07 14:05:05 +0200

    [#] wip

--- HTTP_Server/serverapp/server.js
+++ HTTP_Server/serverapp/server.js
@@ -1,5 +1,5 @@
 // http://localhost:3000/
-// http://localhost:3000/about/
+// http://10.0.1.172:3000/
 //------------------------------------------
 var express = require("express");
 var app = express();
@@ -9,8 +9,8 @@ function start(port) {
 
     // logger. 
     // also use("/", (callback) {response.send();})
-    app.use(function(request, response, next){
-     
+    app.use(function (request, response, next) {
+
         var now = new Date();
         var hour = now.getHours();
         var minutes = now.getMinutes();
@@ -27,20 +27,20 @@ function start(port) {
     app.get("/categories/:categoryId/products/:productId", function (request, response) {
         var catId = request.params["categoryId"];
         var prodId = request.params["productId"];
-      response.send(`Категория: ${catId}    Товар: ${prodId}`);
+        response.send(`Категория: ${catId}    Товар: ${prodId}`);
     });
-
+    // Router with sub route.
     // http://localhost:3000/products
     var productRouter = express.Router();
     app.use("/products", productRouter);
     productRouter.route("/").get(function (request, response) {
 
-            response.send("Список товаров");
-        });
+        response.send("Список товаров");
+    });
     productRouter.route("/:id").get(function (request, response) {
 
-            response.send(`Товар ${request.params.id}`);
-        });
+        response.send(`Товар ${request.params.id}`);
+    });
     app.get("/", function (request, response) {
 
         response.send("Главная страница");
